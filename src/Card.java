c47e13cf5d2eb49340418843ea49e9ea18a0684f
vtaira <veronica.taira@menloschool.org>
2024-11-18 13:04:41 -0800

    added to Player class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
 public class Card {
+    private String rank;
+    private String suit;
+    private int value;
+
+
+    public Card(int value, String suit, String rank) {
+        this.value = value;
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString(String rank, String suit) {
+        return rank + " of " + suit;
+    }
 }
---
+++ src/Player.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private int points;
+    private ArrayList<Card> hand;
+
+
+    public Player(String name) {
+        this.name = name;
+        points = 0;
+    }
+
+    public Player(String name, ArrayList<Card> hand) {
+        this.name = name;
+        this.hand = hand;
+        points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void addPoints(int add)
+    {
+        points += add;
+    }
+
+    public void addCard (Card newCard)
+    {
+        hand.add(newCard);
+    }
+    
+    public String toString(String name, ArrayList<Card> hand, int points){
+        String s = name + " has " + points + " points" + "/n" + name + "'s cards: " + hand;
+        return s;
+    }
+}
